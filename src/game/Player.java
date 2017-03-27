4bcb1800dc7985df750eb8e49b401f3fc53c3285
kais1995 <kaiskais1995@gmail.com>
2017-03-27 16:11:50 -0400

    Update Player.java

--- src/game/Player.java
+++ src/game/Player.java
@@ -18,8 +18,7 @@ public class Player extends GameObject{
     
     public Player(int x, int y, ID id) {
         super(x, y, id);
-        velX = r.nextInt(5) + 1;
-        velY = r.nextInt(5);
+
 
     }
     
@@ -29,7 +28,8 @@ public class Player extends GameObject{
     }
     
     public void render(Graphics g){ //the box in the game
-        g.setColor(Color.white);
+        if (id == ID.Player) g.setColor(Color.white);
+        else if (id == ID.Player2) g.setColor(Color.blue);
         g.fillRect(x, y, 32, 32);
         
     }
